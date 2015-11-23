4a625ca03e7464388651aac0efb7499981bd3875
wookee55 <wookee55@live.com>
2015-11-23 06:40:05 +0000

    Lesson 17 Building Lessons

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Flixter::Application.routes.draw do
   resources :courses, :only => [:index, :show]
 
   namespace :instructor do
+    resources :sections, :only => [] do
+      resources :lessons, :only => [:new, :create]
+    end
     resources :courses, :only => [:new, :create, :show] do
       resources :sections, :only => [:new, :create]
     end
