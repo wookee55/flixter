2edb1bb5cf5d5dc071aa3ba1a8272e3c9e0f0350
wookee55 <wookee55@live.com>
2015-11-24 07:06:04 +0000

    Lesson 19 Dealing with the rest of the Permissions

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -1,5 +1,6 @@
 class Instructor::CoursesController < ApplicationController
   before_action :authenticate_user!
+  before_action :require_authorized_for_current_course, :only => [:show]
 
   def new
     @course = Course.new
@@ -16,7 +17,19 @@ class Instructor::CoursesController < ApplicationController
   end
 
   def show
-    @course = Course.find(params[:id])
+  end
+
+  private
+
+  helper_method :current_course
+  def current_course
+    @current_course ||= Course.find(params[:id])
+  end
+
+  def require_authorized_for_current_course
+    if current_course.user != current_user
+      render :text => "Unauthorized", :status => :unauthorized
+    end
   end
 
   def course_params
