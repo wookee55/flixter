3520aed406a090c76acde4f7337773d914522966
wookee55 <wookee55@live.com>
2015-11-30 06:02:36 +0000

    Lesson 31 [Self Directed] - Reordering sections

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -1,6 +1,7 @@
 class Instructor::SectionsController < ApplicationController
   before_action :authenticate_user!
-  before_action :require_authorized_for_current_course
+  before_action :require_authorized_for_current_course, :only => [:new, :create]
+  before_action :require_authorized_for_current_section, :only => [:update]
 
   def new
     @section = Section.new
@@ -12,11 +13,20 @@ class Instructor::SectionsController < ApplicationController
     redirect_to instructor_course_path(current_course)
   end
 
+  def update
+    current_section.update_attributes(section_params)
+    render :text => 'updated!'
+  end
+
   private
 
   helper_method :current_course
   def current_course
     @current_course ||= Course.find(params[:course_id])
+  end
+
+  def current_section
+    @current_section ||= Section.find(params[:id])
   end
 
   def require_authorized_for_current_course
@@ -25,7 +35,13 @@ class Instructor::SectionsController < ApplicationController
     end
   end
 
+  def require_authorized_for_current_section
+    if current_section.course.user != current_user
+      return render :text => 'Unauthorized', :status => :unauthorized
+    end
+  end
+
   def section_params
-    params.require(:section).permit(:title)
+    params.require(:section).permit(:title, :row_order_position)
   end
 end
